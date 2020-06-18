a4c3d89dc481ed8b81993119ceb9cc4b126e34a7
Damian Garza <damiangarz@gmail.com>
2020-06-18 08:50:09 -0500

    Adding Screenshot and Datadriven features to the FW

--- test_data/SearchItemTestData.py
+++ test_data/SearchItemTestData.py
@@ -1,6 +1,19 @@
-class SearchItemTestData:
-    search_item_data_set = [{'search_item': 'ps4 console'},
-                            {'search_item': 'xbox console'},
-                            {'search_item': 'nintendo switch console'}]
-    search_item_data = [{'search_item': 'ps4 console'}]
+import openpyxl
 
+
+class SearchItemTestData:
+    search_item_data_set = [{'search_item': 'ps4 consola'},
+                            {'search_item': 'x box consola'},
+                            {'search_item': 'nintendo switch consola'}]
+    search_item_data = [{'search_item': 'ps4 consola'}]
+
+    @staticmethod
+    def get_excel_data(test_case_name):
+        book = openpyxl.load_workbook("C:\\Frameworks\\pythonSelFW1\\data_sheet.xlsx")
+        sheet = book.active
+        data_dict = {}
+        for i in range(1, sheet.max_row + 1):
+            if sheet.cell(row=i, column=1).value == test_case_name:
+                for j in range(2, sheet.max_column + 1):
+                    data_dict[sheet.cell(row=1, column=j).value] = sheet.cell(row=i, column=j).value
+        return [data_dict]
--- tests/test_SearchOneItem.py
+++ tests/test_SearchOneItem.py
@@ -5,13 +5,14 @@ import pytest
 
 
 class TestOne(BaseClass):
-    @pytest.fixture(params=SearchItemTestData.search_item_data_set, scope="session")
+    @pytest.fixture(params=SearchItemTestData.get_excel_data('TC1'), scope='session')
     def get_data(self, request):
         return request.param
 
+    @pytest.mark.one
     def test_one(self, get_data):
         log = self.get_logger()
-        log.info("Starting Test")
+        log.debug('Starting Test')
         home_page = HomePage(self.driver, )
         home_page.textbox_item().send_keys(get_data['search_item'])
         results_page = home_page.click_on_search_btn_item()
@@ -24,7 +25,7 @@ class TestOne(BaseClass):
         price_item_page = BaseClass.remove_char(self, price_item_page)
         price_cart_page = BaseClass.remove_char(self, price_cart_page)
         log.info('%s %s %s %s', 'Prices with chars removed:',  price_item_page, 'To be compared with:', price_cart_page)
-        assert price_cart_page == price_item_page
         cart_page.delete_btn_item().click()
-        log.info("Validating Cart displays 0 items at end of test")
-        assert "0" == home_page.cart_icon_item().text
+        assert price_cart_page == price_item_page
+        assert '0' == home_page.cart_icon_item().text
+        log.debug('Completed Test')
